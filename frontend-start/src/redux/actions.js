8d374fdbefa9e96a59b5e67ba98479d7bb092e0c
yoneshinzato <chibariyo@gmail.com>
2018-12-03 22:22:11 -0200

    integração

--- frontend-start/src/redux/actions.js
+++ frontend-start/src/redux/actions.js
@@ -1,89 +1,99 @@
-import axios from 'axios'
+import axios from "axios";
 
 const configuracoes = {
-  baseURL: 'https://reprograma-postit-api.herokuapp.com'
-}
+  baseURL: "http://localhost:5000/api"
+};
 
-const json = localStorage.getItem('usuario')
+const json = localStorage.getItem("usuario");
 if (json) {
-  const usuario = JSON.parse(json)
+  const usuario = JSON.parse(json);
   configuracoes.headers = {
-    'Authorization': usuario.token
-  }
+    "x-access-token": usuario.token
+  };
 }
 
-const api = axios.create(configuracoes)
+const api = axios.create(configuracoes);
 
 export function logaUsuario(dados) {
-  return (dispatch) => {
+  return dispatch => {
+    const json = {
+      email: dados.email,
+      password: dados.senha
+    };
     api
-      .post('/login', dados)
+      .post("/login", json)
       .then(response => {
-        api.defaults.headers.common['Authorization'] = response.data.usuario.token
-        dispatch({ type: 'LOGA_USUARIO', dados: response.data.usuario })
+        api.defaults.headers.common["x-access-token"] = response.data.token;
+        dispatch({ type: "LOGA_USUARIO", dados: response.data });
       })
       .catch(error => {
-        console.error(error)
+        console.error(error);
         // if (error.response) {
         //   alert(error.response.data.erro)
         // }
-        alert('Email ou usuário invalido!')
-      })
-  }
+        alert("Email ou usuário invalido!");
+      });
+  };
 }
 
 export function deslogaUsuario() {
   return {
-    type: 'DESLOGA_USUARIO'
-  }
+    type: "DESLOGA_USUARIO"
+  };
 }
 
 export function cadastraPostit(dados) {
-  return (dispatch) => {
-    api
-      .post('/postits', dados)
-      .then(response => {
-        dados.id = response.data.id
-        dispatch({ type: 'CADASTRA_POSTIT', dados })
-      })
-  }
+  return dispatch => {
+    const json = {
+      title: dados.titulo,
+      description: dados.texto
+    };
+    api.post("/postits", json).then(response => {
+      dados.id = response.data.id;
+      dispatch({ type: "CADASTRA_POSTIT", dados });
+    });
+  };
 }
 
 export function alteraPostit(dados) {
-  return (dispatch) => {
-    const url = `/postits/${dados.id}`
-    api
-      .put(url, dados)
-      .then(() => {
-        dispatch({ type: 'ALTERA_POSTIT', dados })
-      })
-  }
+  return dispatch => {
+    const url = `/postits/${dados.id}`;
+    const json = {
+      title: dados.titulo,
+      description: dados.texto
+    };
+    api.put(url, json).then(() => {
+      dispatch({ type: "ALTERA_POSTIT", dados });
+    });
+  };
 }
 
 export function removePostit(id) {
-  return (dispatch) => {
-    const url = `/postits/${id}`
-    api
-      .delete(url)
-      .then(() => {
-        dispatch({ type: 'REMOVE_POSTIT', id })
-      })
-  } 
+  return dispatch => {
+    const url = `/postits/${id}`;
+    api.delete(url).then(() => {
+      dispatch({ type: "REMOVE_POSTIT", id });
+    });
+  };
 }
 
 export function listaPostits() {
-  return (dispatch) => {
-    api
-      .get('/postits')
-      .then(response => {
-        dispatch({ type: 'LISTA_POSTITS', dados: response.data.postits })
-      })
-  }
+  return dispatch => {
+    api.get("/postits").then(response => {
+      const dados = response.data.map(item => ({
+        //quer modificar a lista que foi recebida
+        id: item.id,
+        titulo: item.title,
+        texto: item.description
+      }));
+      dispatch({ type: "LISTA_POSTITS", dados });
+    });
+  };
 }
 
 export function alteraFiltro(texto) {
   return {
-    type: 'ALTERA_FILTRO', 
+    type: "ALTERA_FILTRO",
     texto
-  }
+  };
 }
