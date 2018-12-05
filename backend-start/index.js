7b5af846c19d8e5945d665e9cf9f1ca8cfb54b47
yoneshinzato <chibariyo@gmail.com>
2018-12-05 21:26:02 -0200

    the end

--- backend-start/index.js
+++ backend-start/index.js
@@ -49,15 +49,21 @@ app.post("/api/login", (req, res) => {
 });
 
 function authenticatesUser(authUser, cb) {
-  const user = users.find(user => user.email === authUser.email);
-
-  if (!user) {
-    return cb({ code: 500, message: "Usuário não existe." });
-  } else if (user.password !== authUser.password) {
-    return cb({ code: 500, message: "Senha incorreta." });
-  }
-
-  return cb(null, user.id);
+  users.findOne(
+    {
+      email: authUser.email,
+      password: authUser.password
+    },
+    function(error, response) {
+      if (error) {
+        return cb({ code: 500, message: error.message });
+      } else if (response === null) {
+        return cb({ code: 500, message: "Usuário ou senha inválido" });
+      } else {
+        return cb(null, response.id);
+      }
+    }
+  );
 }
-
+//cb é callback
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}...`));
--- backend-start/users/users.js
+++ backend-start/users/users.js
@@ -1,37 +1,53 @@
-const postits = require('../postits/postits.js');
+// const postits = require('../postits/postits.js');
 
-const users = [
+// const users = [
+//   {
+//     id: 1,
+//     name: 'Anna Flávia',
+//     email: 'annaflaviac@me.com',
+//     password: 'r3pr0gr@m@2018',
+//     roles: ['admin'],
+//     postits: [postits[0], postits[1]],
+//   },
+//   {
+//     id: 2,
+//     name: 'Ana Paula',
+//     email: 'ana.paula@reprograma.com.br',
+//     password: 'r3pr0gr@m@2017',
+//     roles: [],
+//     postits: [postits[2]],
+//   },
+//   {
+//     id: 3,
+//     name: 'Silvia',
+//     email: 'silvia@reprograma.com.br',
+//     password: 'r3pr0gr@m@2016',
+//     roles: [],
+//   },
+//   {
+//     id: 4,
+//     name: 'Administrador',
+//     email: 'admin@email.com',
+//     password: '123456',
+//     roles: ['admin'],
+//     postits: []
+//   }
+// ];
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+let UserSchema = new Schema(
   {
-    id: 1,
-    name: 'Anna Flávia',
-    email: 'annaflaviac@me.com',
-    password: 'r3pr0gr@m@2018',
-    roles: ['admin'],
-    postits: [postits[0], postits[1]],
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    email: { type: String, unique: true },
+    password: { type: String }
   },
   {
-    id: 2,
-    name: 'Ana Paula',
-    email: 'ana.paula@reprograma.com.br',
-    password: 'r3pr0gr@m@2017',
-    roles: [],
-    postits: [postits[2]],
-  },
-  {
-    id: 3,
-    name: 'Silvia',
-    email: 'silvia@reprograma.com.br',
-    password: 'r3pr0gr@m@2016',
-    roles: [],
-  },
-  {
-    id: 4,
-    name: 'Administrador',
-    email: 'admin@email.com',
-    password: '123456',
-    roles: ['admin'],
-    postits: []
+    collection: "users",
+    versionKey: false
   }
-];
+);
+
+let users = mongoose.model("users", UserSchema);
 
 module.exports = users;
